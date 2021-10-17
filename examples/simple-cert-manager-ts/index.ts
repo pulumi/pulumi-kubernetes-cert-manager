ae7d375167c1c128183aa944acc7785225791733
Joe Duffy <joe@pulumi.com>
2021-10-17 12:41:24 -0700

    Add a cert manager example
    This example installs CertManager into a Kubernetes cluster,
    and then configures a self-signed CA, per
    https://cert-manager.io/docs/configuration/selfsigned/.

---
+++ examples/simple-cert-manager-ts/index.ts
@@ -0,0 +1,35 @@
+import * as k8s from "@pulumi/kubernetes";
+import * as certmanager from "@pulumi/chart-cert-manager";
+
+// Create a sandbox namespace.
+const nsName = "sandbox";
+const ns = new k8s.core.v1.Namespace("sandbox-ns", {
+    metadata: { name: nsName },
+});
+
+// Set up a cert manager namespace and deploy into it.
+const manager = new certmanager.CertManager("cert-manager", {
+    installCRDs: true,
+    helmOptions: {
+        namespace: nsName,
+    },
+});
+
+// Create a cluster issuer that uses self-signed certificates.
+// This is not very secure, but has the least amount of external
+// dependencies, so is simple. Please refer to
+// https://cert-manager.io/docs/configuration/selfsigned/
+// for additional details on other signing providers.
+const issuer = new k8s.apiextensions.CustomResource("issuer", {
+    apiVersion: "cert-manager.io/v1",
+    kind: "Issuer",
+    metadata: {
+        name: "selfsigned-issuer",
+        namespace: nsName,
+    },
+    spec: {
+        selfSigned: {},
+    },
+});
+
+export const certManagerStatus = manager.status;
