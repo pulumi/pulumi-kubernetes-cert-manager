e9fc78dba5f9a4d95aa3019443eb52a2e4302318
Mark Jan van Kampen <m.vankampen@freeday.ai>
2024-01-25 09:44:26 +0100

    Update chart version

--- provider/pkg/provider/chart.go
+++ provider/pkg/provider/chart.go
@@ -16,9 +16,9 @@ package provider
 
 import (
 	helmbase "github.com/pulumi/pulumi-go-helmbase"
-	appsv1 "github.com/pulumi/pulumi-kubernetes/sdk/v4/go/kubernetes/apps/v1"
-	corev1 "github.com/pulumi/pulumi-kubernetes/sdk/v4/go/kubernetes/core/v1"
-	helmv3 "github.com/pulumi/pulumi-kubernetes/sdk/v4/go/kubernetes/helm/v3"
+	appsv1 "github.com/pulumi/pulumi-kubernetes/sdk/v3/go/kubernetes/apps/v1"
+	corev1 "github.com/pulumi/pulumi-kubernetes/sdk/v3/go/kubernetes/core/v1"
+	helmv3 "github.com/pulumi/pulumi-kubernetes/sdk/v3/go/kubernetes/helm/v3"
 
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
