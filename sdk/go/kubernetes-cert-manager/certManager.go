52aa08e806012bfc99690b77026581cd819c2d9c
Mark Jan van Kampen <m.vankampen@freeday.ai>
2024-01-25 09:17:03 +0100

    Updates Go pulumi-kubernetes to v4

--- sdk/go/kubernetes-cert-manager/certManager.go
+++ sdk/go/kubernetes-cert-manager/certManager.go
@@ -7,8 +7,8 @@ import (
 	"context"
 	"reflect"
 
-	appsv1 "github.com/pulumi/pulumi-kubernetes/sdk/v3/go/kubernetes/apps/v1"
-	corev1 "github.com/pulumi/pulumi-kubernetes/sdk/v3/go/kubernetes/core/v1"
+	appsv1 "github.com/pulumi/pulumi-kubernetes/sdk/v4/go/kubernetes/apps/v1"
+	corev1 "github.com/pulumi/pulumi-kubernetes/sdk/v4/go/kubernetes/core/v1"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
