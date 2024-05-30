f1bbe1b278966ab22c042274b3bf9409bc96ea9c
Ben Lewis <blewis@hirundo.io>
2024-05-30 10:51:32 +0300

    Update example to use new resources and meta class

--- examples/simple-cert-manager-py/__main__.py
+++ examples/simple-cert-manager-py/__main__.py
@@ -1,7 +1,8 @@
 import pulumi
-from pulumi_kubernetes.apiextensions import CustomResource
 from pulumi_kubernetes.core.v1 import Namespace
+from pulumi_kubernetes.meta.v1 import ObjectMetaArgs
 from pulumi_kubernetes_cert_manager import CertManager, ReleaseArgs
+from pulumi_cert_manager_resources.certmanager.v1 import Issuer, IssuerSpecArgs
 
 # Create a sandbox namespace.
 ns_name = 'sandbox'
@@ -20,16 +21,14 @@ manager = CertManager('cert-manager',
 # dependencies, so is simple. Please refer to
 # https://cert-manager.io/docs/configuration/selfsigned/
 # for additional details on other signing providers.
-issuer = CustomResource('issuer',
-    api_version='cert-manager.io/v1',
-    kind='Issuer',
-    metadata={
-        'name': 'selfsigned-issuer',
-        'namespace': ns_name,
-    },
-    spec={
-        'selfSigned': {},
-    },
+issuer = Issuer(
+    metadata=ObjectMetaArgs(
+        name='selfsigned-issuer',
+        namespace=ns_name,
+    ),
+    spec=IssuerSpecArgs(
+        selfSigned={},
+    ),
     opts=pulumi.ResourceOptions(depends_on=[manager]),
 )
 
