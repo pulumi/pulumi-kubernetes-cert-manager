9ddb24269fe9d87f1e6312ae954d7323978c4b3a
Richard Shade <richard@pulumi.com>
2024-12-20 11:18:25 -0600

    Adding in test for #133

--- examples/simple-cert-manager-ts/index.ts
+++ examples/simple-cert-manager-ts/index.ts
@@ -6,10 +6,33 @@ const ns = new k8s.core.v1.Namespace("sandbox-ns");
 
 // Install a cert manager into our cluster.
 const manager = new certmanager.CertManager("cert-manager", {
-  installCRDs: true,
-  helmOptions: {
-    namespace: ns.metadata.name,
-  },
+    installCRDs: true,
+    helmOptions: {
+        namespace: ns.metadata.name,
+        version: "v1.15.3",
+    },
+    image: {
+        repository: "public.ecr.aws/eks-anywhere-dev/cert-manager/cert-manager-controller",
+        tag: "v1.15.3-eks-a-v0.21.3-dev-build.0"
+    },
+    cainjector: {
+        "image": {
+            repository: "public.ecr.aws/eks-anywhere-dev/cert-manager/cert-manager-cainjector",
+            tag: "v1.15.3-eks-a-v0.21.3-dev-build.0",
+        },
+    },
+    startupapicheck: {
+        "image": {
+            repository: "public.ecr.aws/eks-anywhere-dev/cert-manager/cert-manager-startupapicheck",
+            tag: "v1.15.3-eks-a-v0.21.3-dev-build.0",
+        }
+    },
+    webhook: {
+        image: {
+            repository: "public.ecr.aws/eks-anywhere-dev/cert-manager/cert-manager-webhook",
+            tag: "v1.15.3-eks-a-v0.21.3-dev-build.0"
+        }
+    }
 });
 
 // Create a cluster issuer that uses self-signed certificates.
@@ -18,19 +41,19 @@ const manager = new certmanager.CertManager("cert-manager", {
 // https://cert-manager.io/docs/configuration/selfsigned/
 // for additional details on other signing providers.
 const issuer = new k8s.apiextensions.CustomResource(
-  "issuer",
-  {
-    apiVersion: "cert-manager.io/v1",
-    kind: "Issuer",
-    metadata: {
-      name: "selfsigned-issuer",
-      namespace: ns.metadata.name,
+    "issuer",
+    {
+        apiVersion: "cert-manager.io/v1",
+        kind: "Issuer",
+        metadata: {
+            name: "selfsigned-issuer",
+            namespace: ns.metadata.name,
+        },
+        spec: {
+            selfSigned: {},
+        },
     },
-    spec: {
-      selfSigned: {},
-    },
-  },
-  { dependsOn: manager }
+    { dependsOn: manager }
 );
 
 export const certManagerStatus = manager.status;
