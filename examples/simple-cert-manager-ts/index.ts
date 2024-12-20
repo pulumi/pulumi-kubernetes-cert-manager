8670e715cfd3c4b259781152cdecb411611e884c
Richard Shade <richard@pulumi.com>
2024-12-20 14:12:30 -0600

    Adding in fix, for repository to take output, fixes #133

--- examples/simple-cert-manager-ts/index.ts
+++ examples/simple-cert-manager-ts/index.ts
@@ -1,38 +1,45 @@
 import * as k8s from "@pulumi/kubernetes";
 import * as certmanager from "@pulumi/kubernetes-cert-manager";
+import * as random from "@pulumi/random";
+
+const randomString = new random.RandomString("random", {
+  length: 16,
+  special: true,
+  overrideSpecial: "/@£$",
+})
 
 // Create a sandbox namespace.
 const ns = new k8s.core.v1.Namespace("sandbox-ns");
 
 // Install a cert manager into our cluster.
 const manager = new certmanager.CertManager("cert-manager", {
-    installCRDs: true,
-    helmOptions: {
-        namespace: ns.metadata.name,
-        version: "v1.15.3",
+  installCRDs: true,
+  helmOptions: {
+    namespace: ns.metadata.name,
+    version: "v1.15.3",
+  },
+  image: {
+    repository: randomString.result.apply(result => "public.ecr.aws/eks-anywhere-dev/cert-manager/cert-manager-controller"),
+    tag: "v1.15.3-eks-a-v0.21.3-dev-build.0"
+  },
+  cainjector: {
+    "image": {
+      repository: "public.ecr.aws/eks-anywhere-dev/cert-manager/cert-manager-cainjector",
+      tag: "v1.15.3-eks-a-v0.21.3-dev-build.0",
     },
-    image: {
-        repository: "public.ecr.aws/eks-anywhere-dev/cert-manager/cert-manager-controller",
-        tag: "v1.15.3-eks-a-v0.21.3-dev-build.0"
-    },
-    cainjector: {
-        "image": {
-            repository: "public.ecr.aws/eks-anywhere-dev/cert-manager/cert-manager-cainjector",
-            tag: "v1.15.3-eks-a-v0.21.3-dev-build.0",
-        },
-    },
-    startupapicheck: {
-        "image": {
-            repository: "public.ecr.aws/eks-anywhere-dev/cert-manager/cert-manager-startupapicheck",
-            tag: "v1.15.3-eks-a-v0.21.3-dev-build.0",
-        }
-    },
-    webhook: {
-        image: {
-            repository: "public.ecr.aws/eks-anywhere-dev/cert-manager/cert-manager-webhook",
-            tag: "v1.15.3-eks-a-v0.21.3-dev-build.0"
-        }
+  },
+  startupapicheck: {
+    "image": {
+      repository: "public.ecr.aws/eks-anywhere-dev/cert-manager/cert-manager-startupapicheck",
+      tag: "v1.15.3-eks-a-v0.21.3-dev-build.0",
     }
+  },
+  webhook: {
+    image: {
+      repository: "public.ecr.aws/eks-anywhere-dev/cert-manager/cert-manager-webhook",
+      tag: "v1.15.3-eks-a-v0.21.3-dev-build.0"
+    }
+  }
 });
 
 // Create a cluster issuer that uses self-signed certificates.
@@ -41,19 +48,19 @@ const manager = new certmanager.CertManager("cert-manager", {
 // https://cert-manager.io/docs/configuration/selfsigned/
 // for additional details on other signing providers.
 const issuer = new k8s.apiextensions.CustomResource(
-    "issuer",
-    {
-        apiVersion: "cert-manager.io/v1",
-        kind: "Issuer",
-        metadata: {
-            name: "selfsigned-issuer",
-            namespace: ns.metadata.name,
-        },
-        spec: {
-            selfSigned: {},
-        },
+  "issuer",
+  {
+    apiVersion: "cert-manager.io/v1",
+    kind: "Issuer",
+    metadata: {
+      name: "selfsigned-issuer",
+      namespace: ns.metadata.name,
     },
-    { dependsOn: manager }
+    spec: {
+      selfSigned: {},
+    },
+  },
+  { dependsOn: manager }
 );
 
 export const certManagerStatus = manager.status;
